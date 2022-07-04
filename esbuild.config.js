c4b9f121fdec1151e9745d5e0eaf85dee7c83d7f
Jo Liss <joliss42@gmail.com>
2022-07-04 23:26:43 +0000

    Add cssnano

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,17 +1,32 @@
 import esbuild from 'esbuild';
 import postcss from 'esbuild-postcss';
 
-const host = '127.0.0.1';
-const port = 3000;
-
-console.log(`Serving on http://${host}:${port}`);
-
-esbuild.serve({
-  host: host,
-  port: port,
-  servedir: 'public',
-}, {
+const esbuildOptions = {
   entryPoints: ['src/app.tsx', 'src/app.css'],
   bundle: true,
   plugins: [postcss()],
-}).catch((err) => process.exit(1));
+};
+
+const host = '127.0.0.1';
+const port = 3000;
+
+const serve = process.argv.indexOf('serve') !== -1;
+if (serve) {
+  console.log(`Serving on http://${host}:${port}`);
+  esbuild.serve({
+    host: host,
+    port: port,
+    servedir: 'public',
+  }, esbuildOptions).catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+} else {
+  esbuild.build({
+    ...esbuildOptions,
+    outdir: 'dist',
+  }).catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+}
---
+++ postcss.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  map: false,
+  plugins: {
+    'postcss-import': {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {},
+    autoprefixer: {},
+    // ...(process.env.NODE_ENV === 'production' ? { cssnano: { preset: 'default', } } : {}),
+    cssnano: { preset: 'default', },
+  },
+}
